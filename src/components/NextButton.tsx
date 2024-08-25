ac51aa80503a25f93d088fe3aae7a99c6f85e298
Krystian <texen24@gmail.com>
2024-08-25 16:49:58 +0200

    finished logic responsible for pagination in PrevButton and NextButton

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -9,24 +9,31 @@ type Params = {
 
 const NextButton = ({ page, path }: Params) => {
   const data = useLoaderData() as LocationOrEpisodeResponse;
-  const itemId = data.singleLocation?.id ?? data.singleEpisode?.id ?? 1;
+  const itemId = data.singleLocation?.id ?? data.singleEpisode?.id;
   const paramId = Number(itemId) % 20 === 0 ? 20 : Number(itemId) % 20;
   const lastItem = data.info?.count;
-console.log(lastItem);
+
   return (
-    <Link
-      to={
-        path === 'character'
-          ? `/${path}s/${page - 1}`
-          : `/${path}s?${path}=${paramId === 20 ? 1 : paramId + 1}&page=${
-              paramId === 20 ? page + 1 : page
-            }`
-      }
-      className='flex items-center py-1 sm:py-2 px-4 w-fit  sm:gap-x-1 bg-slate-900 text-lime-200 rounded-3xl hover:scale-105 duration-500 uppercase'
-    >
-      <span className='text-xl tracking-wider'>next</span>
-      <GrFormNext className='w-14 h-14 lg:w-12 lg:h-12' />
-    </Link>
+    <>
+      {itemId !== undefined &&
+        lastItem !== undefined &&
+        itemId !== lastItem && (
+          <Link
+            to={
+              path === 'character'
+                ? `/${path}s/${page - 1}`
+                : `/${path}s?${path}=${paramId === 20 ? 1 : paramId + 1}&page=${
+                    paramId === 20 ? page + 1 : page
+                  }`
+            }
+            className='flex items-center py-1 sm:py-2 px-4 w-fit sm:gap-x-1 bg-slate-900 text-lime-200 rounded-3xl hover:scale-105 duration-500 uppercase'
+          >
+            <span className='text-xl tracking-wider'>next</span>
+            <GrFormNext className='w-14 h-14 lg:w-12 lg:h-12' />
+          </Link>
+        )}
+    </>
   );
 };
+
 export default NextButton;
--- src/components/PrevButton.tsx
+++ src/components/PrevButton.tsx
@@ -8,22 +8,27 @@ type Params = {
 
 const PrevButton = ({ page, path }: Params) => {
   const data = useLoaderData() as LocationOrEpisodeResponse;
-  const itemId = data.singleLocation?.id ?? data.singleEpisode?.id ?? 1;
+  const itemId = data.singleLocation?.id ?? data.singleEpisode?.id;
   const paramId = Number(itemId) % 20 === 0 ? 20 : Number(itemId) % 20;
+
   return (
-    <Link
-      to={
-        path === 'character'
-          ? `/${path}s/${page - 1}`
-          : `/${path}s?${path}=${paramId === 1 ? 20 : paramId}&page=${
-              paramId === 1 ? page - 1 : page
-            }`
-      }
-      className='flex items-center py-1 sm:py-2 px-4 w-fit  sm:gap-x-1 bg-slate-900 text-lime-200 rounded-3xl hover:scale-105 duration-500 uppercase'
-    >
-      <GrFormPrevious className='w-14 h-14 lg:w-12 lg:h-12' />
-      <span className='text-xl tracking-wider'>prev</span>
-    </Link>
+    <>
+      {itemId !== 1 && (
+        <Link
+          to={
+            path === 'character'
+              ? `/${path}s/${page - 1}`
+              : `/${path}s?${path}=${paramId === 1 ? 20 : paramId - 1}&page=${
+                  paramId === 1 ? page - 1 : page
+                }`
+          }
+          className='flex items-center py-1 sm:py-2 px-4 w-fit sm:gap-x-1 bg-slate-900 text-lime-200 rounded-3xl hover:scale-105 duration-500 uppercase'
+        >
+          <GrFormPrevious className='w-14 h-14 lg:w-12 lg:h-12' />
+          <span className='text-xl tracking-wider'>prev</span>
+        </Link>
+      )}
+    </>
   );
 };
 export default PrevButton;
